0d1960881a28b81162e3c493bf1dfb600cca5118
Chris Noble <chris.noble@hootsuite.com>
2014-05-10 16:41:52 -0700

    Range updated

--- www/js/views/question-view.js
+++ www/js/views/question-view.js
@@ -12,6 +12,7 @@ var QuestionView = Backbone.View.extend({
 
 
     events:{
+        'change input[type="range"]': 'rangeChange',
         // 'click #hootShareIntent': 'intentShare',
         // 'click #hootSharePost':'postShare',
         // 'focus #pagelet_composer textarea':'textAreaFocus'
@@ -25,7 +26,7 @@ var QuestionView = Backbone.View.extend({
     initialize : function(options) {
 
         //Bind this to the following functions
-        _.bindAll(this, "render");
+        _.bindAll(this, "render", "rangeChange");
 
         var that = this;
         if (options.model) {
@@ -44,6 +45,25 @@ var QuestionView = Backbone.View.extend({
         var rendered = Mustache.to_html(templater, this.model.toJSON());
         this.$el.html(rendered);
         return this;
+    },
+
+    /**
+    * rangeChange
+    * Method for changing the range value
+    * @param jQuery Event
+    */
+    rangeChange: function(event) {
+        var $range = $(event.currentTarget);
+        var val = ($range.val() - $range.attr('min')) / ($range.attr('max') - $range.attr('min'));
+
+        $range.css('background-image',
+            '-webkit-gradient(linear, left top, right top, '
+            + 'color-stop(' + val + ', #1d6ab7), '
+            + 'color-stop(' + val + ', #d2e1F1)'
+            + ')'
+        );
+
+        this.$('.value').html($range.val() );
     }
 
 
